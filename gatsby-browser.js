4f4a5daefdf88340f25f232b25393b49fe5c8fda
mrprofessor <kar.rudra008@gmail.com>
2019-11-30 01:32:05 +0530

    Added different glitch machine

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,5 @@
 import "./src/styles/global.scss";
 import "./src/styles/fav_font.css";
-import "typeface-overpass-mono";
-//import "typeface-roboto-mono";
-//import "typeface-roboto";
-//import "typeface-overpass";
-//import "typeface-open-sans";
 
 import "prismjs/themes/prism-tomorrow.css";
 //import "prismjs/themes/prism-okaidia.css";
